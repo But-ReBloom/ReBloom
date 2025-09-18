475a8ec289bc1cd360c7437c3a58e72821e5ea61
이도연 <dgsw0509@dgsw.hs.kr>
2025-09-18 23:44:50 +0900

    Initial Commit

---
+++ src/main/java/com/but/rebloom/RebloomApplication.java
@@ -0,0 +1,13 @@
+package com.but.rebloom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebloomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebloomApplication.class, args);
+	}
+
+}
