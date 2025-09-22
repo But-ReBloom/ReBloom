cacccbce4f2a2fb55092a4e86666456dfd3ddbc5
황정빈 <hjbin1211@gmail.com>
2025-09-22 23:23:15 +0900

    refactor : Auth 경로 수정

---
+++ backend/src/main/java/com/but/rebloom/auth/usecase/SignupUseCase.java
@@ -0,0 +1,11 @@
+package com.but.rebloom.auth.usecase;
+
+import com.but.rebloom.auth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SignupUseCase {
+    private final UserRepository userRepository;
+}
