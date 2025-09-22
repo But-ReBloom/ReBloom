32f53cf23bbbbd283974870ef6f173a937dc6754
황정빈 <hjbin1211@gmail.com>
2025-09-23 08:41:19 +0900

    feat : ExceptionHandler 추가

---
+++ backend/src/main/java/com/but/rebloom/auth/usecase/LoginUseCase.java
@@ -0,0 +1,12 @@
+package com.but.rebloom.auth.usecase;
+
+import com.but.rebloom.auth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginUseCase {
+    private final UserRepository userRepository;
+    private final ValidationUseCase validationUseCase;
+}
