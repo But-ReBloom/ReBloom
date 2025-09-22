eaec0c07a606bc80d5bc1a1c544a87e76961afcb
황정빈 <hjbin1211@gmail.com>
2025-09-22 21:48:14 +0900

    temp commit

---
+++ backend/src/main/java/com/but/rebloom/ReBloomApplication.java
@@ -0,0 +1,13 @@
+package com.but.rebloom;
+s
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReBloomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReBloomApplication.class, args);
+	}
+
+}
