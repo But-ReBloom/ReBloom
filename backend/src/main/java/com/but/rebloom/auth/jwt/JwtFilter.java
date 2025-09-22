75c3c99c1281840caf284c5e157fd382a789ec55
황정빈 <hjbin1211@gmail.com>
2025-09-23 08:53:27 +0900

    feat : LoginUseCase 추가

--- backend/src/main/java/com/but/rebloom/auth/jwt/JwtFilter.java
+++ backend/src/main/java/com/but/rebloom/auth/jwt/JwtFilter.java
@@ -20,19 +20,7 @@ public class JwtFilter extends OncePerRequestFilter {
 
         if (path.equals("/favicon.ico") ||
                 path.startsWith("/.well-known/") ||
-                path.startsWith("/auth/") ||
-                path.startsWith("/email/") ||
-                path.startsWith("/last/") ||
-                path.startsWith("/main/") ||
-                path.startsWith("/img/") ||
-                path.startsWith("/login/") ||
-                path.startsWith("/logout/") ||
-                path.startsWith("/inquiry/") ||
-                path.startsWith("/room/") ||
-                path.startsWith("/class/") ||
-                path.startsWith("/classroom/") ||
-                path.startsWith("/clubroom/") ||
-                path.startsWith("/signup/")) {
+                path.startsWith("/auth/")) {
             filterChain.doFilter(request, response);
             return;
         }
@@ -43,7 +31,7 @@ public class JwtFilter extends OncePerRequestFilter {
             String token = authHeader.substring(7);
             try {
                 jwtAuth.validateToken(token);
-                String userId = jwtAuth.innerIdFromToken(token);
+                String userEmail = jwtAuth.innerIdFromToken(token);
             } catch (Exception e) {
                 response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
                 response.getWriter().write("Invalid JWT token");
