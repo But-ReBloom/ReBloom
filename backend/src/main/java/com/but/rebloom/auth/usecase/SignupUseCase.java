f566fa23136bf17d2e5e53ed814d4921618be01f
황정빈 <hjbin1211@gmail.com>
2025-09-22 23:42:08 +0900

    feat : SignupUseCase 추가

--- backend/src/main/java/com/but/rebloom/auth/usecase/SignupUseCase.java
+++ backend/src/main/java/com/but/rebloom/auth/usecase/SignupUseCase.java
@@ -1,5 +1,7 @@
 package com.but.rebloom.auth.usecase;
 
+import com.but.rebloom.auth.domain.User;
+import com.but.rebloom.auth.dto.request.SignupRequest;
 import com.but.rebloom.auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,4 +12,30 @@ import org.springframework.stereotype.Service;
 public class SignupUseCase {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final ValidationUseCase validationUseCase;
+
+    public void signup(SignupRequest signupRequest) {
+        validationUseCase.checkNull(signupRequest);
+
+        String userEmail = signupRequest.getUserEmail();
+        String userId = signupRequest.getUserId();
+        String userPassword = signupRequest.getUserPassword();
+        String userName = signupRequest.getUserName();
+
+        validationUseCase.checkUserEmail(userEmail);
+        validationUseCase.checkUserId(userId);
+        validationUseCase.checkUserPassword(userPassword);
+        validationUseCase.checkUserName(userName);
+
+        validationUseCase.checkExistAccount(userEmail, userId);
+
+        User user = User.builder()
+                .userEmail(userEmail)
+                .userId(userId)
+                .userPassword(passwordEncoder.encode(userPassword))
+                .userName(userName)
+                .build();
+
+        userRepository.save(user);
+    }
 }
