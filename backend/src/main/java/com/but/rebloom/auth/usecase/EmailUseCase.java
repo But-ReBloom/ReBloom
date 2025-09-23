0216d28bbcf6e608295a49daae06f5fdc5f14c0d
황정빈 <hjbin1211@gmail.com>
2025-09-23 14:13:19 +0900

    feat : EmailUseCase 추가

---
+++ backend/src/main/java/com/but/rebloom/auth/usecase/EmailUseCase.java
@@ -0,0 +1,43 @@
+package com.but.rebloom.auth.usecase;
+
+import com.but.rebloom.auth.dto.request.SendEmailRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+@Service
+@RequiredArgsConstructor
+public class EmailUseCase {
+    private final JavaMailSender mailSender;
+    private final Map<String, String> codeMap = new HashMap<>();
+
+    private String generateCode() {
+        return String.format("%06d", new Random().nextInt(1000000));
+    }
+
+    public String sendVerificationEmail(SendEmailRequest emailRequest) {
+        String code = generateCode();
+        codeMap.put(emailRequest.getUserEmail(), code);
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setTo(emailRequest.getUserEmail());
+        message.setSubject("[이메일 인증 코드]");
+        message.setText("ReBloom 인증코드: " + code);
+
+        mailSender.send(message);
+        return code;
+    }
+
+    public Boolean verifyCode(String email, String inputCode) {
+        if (codeMap.get(email).equals(inputCode)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
