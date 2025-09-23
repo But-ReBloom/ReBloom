e75c0c93a44c1f6c59d027465e259ed1440de071
Divanzo <dhdydwns723@gmail.com>
2025-09-23 16:50:02 +0900

    feat : 메인페이지 제작 및 라우팅 환경 설정 완료

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import Main from './Pages/MainPage'
+import './App.css'
+import { Routes, Route, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    // <Routes>
+    //   <Route path='/Main' element={<Main/>}/>
+    // </Routes>
+    <Main/>
+  );
+}
+
+export default App
