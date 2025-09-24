9f8a94011b8b73d74b7f9653d0edff32c5502e0f
황정빈 <hjbin1211@gmail.com>
2025-09-24 17:32:06 +0900

    feat : UpdateUserInfo query addition

--- backend/src/main/java/com/but/rebloom/auth/repository/UserRepository.java
+++ backend/src/main/java/com/but/rebloom/auth/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.but.rebloom.auth.repository;
 
 import com.but.rebloom.auth.domain.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -14,4 +18,15 @@ public interface UserRepository extends JpaRepository<User, Long> {
     // 유저 존재 확인 함수
     boolean existsByUserId(String userId);
     boolean existsByUserEmail(String userEmail);
+
+    // 유저 아이디 수정
+    @Transactional
+    @Modifying
+    @Query("UPDATE User user SET user.userId = :newId WHERE user.userEmail = :userEmail")
+    void updateUserId(@Param("userEmail") String email, @Param("newId") String userId);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE User user SET user.userId = :newPassword WHERE user.userEmail = :userEmail")
+    void updateUserPassword(@Param("userEmail") String email, @Param("newPassword") String newPassword);
 }
--- backend/src/main/java/com/but/rebloom/auth/usecase/UpdateUserInfoUseCase.java
+++ backend/src/main/java/com/but/rebloom/auth/usecase/UpdateUserInfoUseCase.java
@@ -1,5 +1,6 @@
 package com.but.rebloom.auth.usecase;
 
+import com.but.rebloom.auth.dto.request.SendVerificationEmailRequest;
 import com.but.rebloom.auth.dto.request.UpdateIdRequest;
 import com.but.rebloom.auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,11 +19,21 @@ public class UpdateUserInfoUseCase {
     // 예외 분리
     private final ValidationUseCase validationUseCase;
 
-    public String UpdateUserId(UpdateIdRequest updateIdRequest) {
+    public void hostEmailVerifyWithUserId(UpdateIdRequest updateIdRequest, SendVerificationEmailRequest sendVerificationEmailRequest) {
+        emailUseCase.sendVerificationEmail(sendVerificationEmailRequest);
+    }
+
+    public String updateUserId(UpdateIdRequest updateIdRequest, SendVerificationEmailRequest sendVerificationEmailRequest) {
         String userId = updateIdRequest.getUserId();
 
         validationUseCase.checkNull(userId);
         validationUseCase.checkUserId(userId);
         validationUseCase.checkUserId(userId);
+
+        emailUseCase.sendVerificationEmail(sendVerificationEmailRequest);
+
+        userRepository.updateUserId(sendVerificationEmailRequest.getUserEmail(), userId);
+
+        return "";
     }
 }
