bdf1b20ada490fd44bead01a01629bed21fc90a6
황정빈 <hjbin1211@gmail.com>
2025-09-24 15:02:51 +0900

    feat : ValidationUseCase checkNull overriding

---
+++ backend/src/main/java/com/but/rebloom/auth/usecase/UpdateUserInfoUseCase.java
@@ -0,0 +1,25 @@
+package com.but.rebloom.auth.usecase;
+
+import com.but.rebloom.auth.dto.request.UpdateIdRequest;
+import com.but.rebloom.auth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UpdateUserInfoUseCase {
+    // 디비 접근
+    private final UserRepository userRepository;
+    // 비밀번호 암호화
+    private final PasswordEncoder passwordEncoder;
+    // 이메일 인증
+    private final EmailUseCase emailUseCase;
+    // 예외 분리
+    private final ValidationUseCase validationUseCase;
+
+    public String UpdateUserId(UpdateIdRequest updateIdRequest) {
+//        validationUseCase.
+        return "";
+    }
+}
