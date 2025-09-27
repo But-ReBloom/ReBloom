1ce72b563abad71859983f9a62623f1c2512be20
Divanzo <dhdydwns723@gmail.com>
2025-09-27 19:52:50 +0900

    feat : DS 스토어 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
 import MainPage from './Pages/mainpage/MP'
 import './App.css'
-import { Outlet } from "react-router";
 
 function App() {
   return (   
     <>
     <MainPage/>
-    <Outlet/>
     </>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,10 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom';
 import router from './router.tsx';
 import React from "react";
-import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-    <ToastContainer limit={1} position="top-right" autoClose={2000}/>
-  </React.StrictMode>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
 )
