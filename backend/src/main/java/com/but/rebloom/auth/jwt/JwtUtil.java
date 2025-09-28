a7f56dc0fd3711b9fa47da7a320a29b8f24818f6
황정빈 <hjbin1211@gmail.com>
2025-09-28 14:11:12 +0900

    feat : Checking Current User's email

--- backend/src/main/java/com/but/rebloom/auth/jwt/JwtUtil.java
+++ backend/src/main/java/com/but/rebloom/auth/jwt/JwtUtil.java
@@ -1,6 +1,8 @@
 package com.but.rebloom.auth.jwt;
 
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.security.Key;
 
@@ -12,5 +14,14 @@ public class JwtUtil {
     public JwtUtil(String secretKey, Long expiration) {
         this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
         this.expiration = expiration;
+    }
+
+    public String getCurrentUserEmail() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            // 이상하지만 안에 이메일 있음
+            return authentication.getName();
+        }
+        return null;
     }
 }
