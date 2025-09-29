2708815d4f25b912f08ee32f838e967f6cf0c0e0
이도연 <dgsw0509@dgsw.hs.kr>
2025-09-29 23:47:01 +0900

    feat : heart and comment request

--- backend/src/main/java/com/but/rebloom/channel/dto/request/HeartRequest.java
+++ backend/src/main/java/com/but/rebloom/channel/dto/request/HeartRequest.java
@@ -1,4 +1,15 @@
 package com.but.rebloom.channel.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class HeartRequest {
+    @NotNull
+    private Long postId;
+
+    @NotNull
+    private String userId;
 }
--- backend/src/main/java/com/but/rebloom/channel/dto/request/ReactPostRequest.java
+++ backend/src/main/java/com/but/rebloom/channel/dto/request/ReactPostRequest.java
@@ -1,4 +1,18 @@
 package com.but.rebloom.channel.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ReactPostRequest {
+    @NotNull
+    private Long postId;
+
+    @NotNull
+    private String userId;
+
+    @NotNull
+    private String commentContent;
 }
