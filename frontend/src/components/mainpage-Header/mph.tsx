7d54f3b570dc964e935dd42429a00b040be07fea
Divanzo <dhdydwns723@gmail.com>
2025-09-30 16:44:17 +0900

    feat : made_menu_bar (component)

--- frontend/src/components/mainpage-Header/mph.tsx
+++ frontend/src/components/mainpage-Header/mph.tsx
@@ -1,6 +1,7 @@
 import logo from '../../assets/images/Rebloom-logo.svg';
 import { Link } from "react-router-dom";
 import * as S from './style.ts';
+import Menu_Bar from '../menu-bar/mb.tsx';
 
 function Header() {
     //메인 페이지_헤더
@@ -9,14 +10,7 @@ function Header() {
             <S.HeaderContainer>
                 <Link to="/"><S.Logo_svg src={logo}/></Link>
                 <S.HeaderRight>
-                        <div className='header-menu'>
-                            <ul>
-                                <li><a href="">취향 발견</a></li>
-                                <li><a href="">취향 탐색</a></li>
-                                <li><a href="">커뮤니티</a></li>
-                                <li><a href="">마이페이지</a></li>
-                            </ul>
-                        </div>
+                        <Menu_Bar/>
                         <div className="go-login-button">
                             <Link to="/login"><button className='login-button-go-lp' id='login-box_button'>Login</button></Link>
                         </div>
