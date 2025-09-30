2abff41a93c2dbda2ede97e8720a3ee6218c149c
leemsgm09 <leemsgm09@dgsw.hs.kr>
2025-09-30 19:58:08 +0900

    feat : 취향테스트 메인페이지 바디 제작

---
+++ frontend/src/components/findtaste-Body/ftb.tsx
@@ -0,0 +1,22 @@
+import * as S from "./style.ts";
+
+function FT_Body() {
+    return (
+    <>
+        <S.Center>
+            <S.Title_box>
+                <S.Title>나만의 취향을 발견할 준비 되셨나요?</S.Title>
+                <S.Blue_Line/>
+            </S.Title_box>
+            <S.subTitle_box>
+                <S.subTitle>숨겨진 성향과 스타일을 찾아보세요.</S.subTitle>
+                <S.Connect_Button onClick={() => (window.location.href = "/taste/hobby/start")}>
+                취향테스트 하기 <span className="arrow">{">"}</span>
+                </S.Connect_Button>
+            </S.subTitle_box>
+        </S.Center>
+    </>
+    );
+}
+
+export default FT_Body;
