b279ea5987bff2a898a112199904ce9a99c6acf2
leemsgm09 <leemsgm09@dgsw.hs.kr>
2025-09-30 19:56:49 +0900

    feat : 취향테스트  기초 틀 제작

---
+++ frontend/src/Pages/findtaste-description-page/ft-dp.tsx
@@ -0,0 +1,12 @@
+import FT_Header from "../../components/findtaste-Header/fth.tsx";
+import * as S from "./style.ts";
+
+function FT_HobbyTest_Description() {
+    return (
+            <S.Header>
+                <FT_Header/>
+            </S.Header>
+    )
+};
+
+export default FT_HobbyTest_Description;
