28192999dfeab2d88707025d34a4830373c3944a
Divanzo <dhdydwns723@gmail.com>
2025-09-30 16:44:17 +0900

    feat : made_menu_bar (component)

---
+++ frontend/src/components/menu-bar/mb.tsx
@@ -0,0 +1,16 @@
+function Menu_Bar(){
+    return(
+    <>
+        <div className='header-menu'>
+            <ul>
+                <li><a href="">취향 발견</a></li>
+                <li><a href="">취향 탐색</a></li>
+                <li><a href="">커뮤니티</a></li>
+                <li><a href="">마이페이지</a></li>
+            </ul>
+        </div>
+    </>
+    );
+};
+
+export default Menu_Bar;
