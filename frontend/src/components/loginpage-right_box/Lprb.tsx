58ab80433eca60411f1725b5b53f726c74d7d8e8
Divanzo <dhdydwns723@gmail.com>
2025-10-22 11:55:27 +0900

    fix : 데이터 요청문 try -> 표준문

--- frontend/src/components/loginpage-right_box/Lprb.tsx
+++ frontend/src/components/loginpage-right_box/Lprb.tsx
@@ -13,47 +13,38 @@ export default function Right_box() {
   const [password, setPassword] = useState("");
 
   // 로그인 함수
-  const handleSubmit = async () => {
+  const handleSubmit = () => {
     // 로그인 영역 , 비밀번호 영역 중 하나라도 공백이면 에러
     if (!userEmail.trim() || !password.trim()) {
       toast.error("빈 칸이 있어선 안됩니다.");
       return;
     }
 
-    // 그게 아니라면 바로 리스폰스 시도
-    try {
-      const response = await fetch("", {
-        method: "POST",
-        body: JSON.stringify({
-          userEmail: userEmail,
-          userPassword: password,
-          provider: "SELF",
-        }),
+    fetch("", {
+      method: "POST",
+      body: JSON.stringify({
+        userEmail: userEmail,
+        userPassword: password,
+        provider: "SELF",
+      }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! 현 상태: ${response.status}`);
+        }else{
+          return response.json();
+        }
+      })
+      .then((data) => {
+        toast.success(<>환영합니다! {data.userName}님 <br /> 이동 중입니다...</>);
+        setTimeout(() => {
+          navigate("/", { replace: true });
+        }, 2000);
+      })
+      .catch((error) => {
+        console.error("Fetch error:", error);
+        toast.error(<>로그인에 실패했습니다. 다시 시도해주세요.</>);
       });
-
-      // 주소가 알맞지 않다면 에러 처리
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-
-      // 데이터 가공
-      const data = await response.json();
-      // console.log("서버 응답:", data);
-
-      // 로그인 성공 시
-      toast.success(
-        <>
-          환영합니다! {data.userName}님 <br /> 이동 중입니다...
-        </>
-      );
-      setTimeout(() => {
-        navigate("/", { replace: true });
-      }, 2000);
-    } catch (error) {
-      // 잘못된 정보인 데이터가 확인된다면  처리
-      console.error("Fetch error:", error);
-      toast.error(<>로그인에 실패했습니다. 다시 시도해주세요.</>);
-    }
   };
 
   return (
