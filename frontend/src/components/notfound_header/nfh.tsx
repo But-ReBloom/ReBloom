fccc8f0b0d0443d4fc7cfc8c64418207554ad3a7
Divanzo <dhdydwns723@gmail.com>
2025-10-26 11:43:33 +0900

    feature : 나가기 버튼 추가 완료

--- frontend/src/components/notfound_header/nfh.tsx
+++ frontend/src/components/notfound_header/nfh.tsx
@@ -1,16 +1,24 @@
-import logo from '../../assets/images/Rebloom-logo.svg';
+import Logo from "../../assets/images/Rebloom-logo.svg";
 import { Link } from "react-router-dom";
-import * as S from './style.ts';
+import * as S from "./style.ts";
+import Exit from "../../assets/images/exit.svg";
 
 function Header() {
-    //메인 페이지_헤더
-    return (
-        <>
-            <S.HeaderContainer>
-                <Link to="/"><S.Logo_svg src={logo}/></Link>
-            </S.HeaderContainer>
-        </>
-    );
+  //메인 페이지_헤더
+  return (
+    <>
+      <S.HeaderContainer>
+        <Link to="/">
+          <S.Logo_svg src={Logo} />
+        </Link>
+        <Link to="/">
+          <S.ExitBtn>
+            <S.Exit_img src={Exit} alt="나가기 버튼" />
+          </S.ExitBtn>
+        </Link>
+      </S.HeaderContainer>
+    </>
+  );
 }
 
-export default Header
+export default Header;
