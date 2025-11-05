83dee1c5c0b9909abb387d6e6096b688c17a7439
Divanzo <dhdydwns723@gmail.com>
2025-11-05 13:45:01 +0900

    fix : UI 뷰 설정 코드 수정

--- frontend/src/components/myexps_detail/style.ts
+++ frontend/src/components/myexps_detail/style.ts
@@ -11,9 +11,23 @@ export const Wrapper = styled.div`
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
   width: 1200px;
   height: 640px;
   background-color: rgb(255, 255, 255, 0.58);
   border-radius: 20px;
+  box-shadow: 0px 0px 40px rgba(0, 0, 0, 0.12);
+`;
+
+export const Arrow = styled.button`
+  margin-right: auto;
+  margin-left: 16px;
+  margin-top: 16px;
+  background: none;
+  border: none;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  transform: rotate(180deg);
+  &:hover {
+    transform: rotate(180deg) scale(1.2);
+  }
 `;
