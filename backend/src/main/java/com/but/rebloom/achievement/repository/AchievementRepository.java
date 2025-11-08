2df5a6120c788a3eb9ce5c30e283ad447a3a03a2
황정빈 <hjbin1211@gmail.com>
2025-11-09 01:59:58 +0900

    fix : Fix other settings (includes JwtFilter, SecurityConfig, AchievementController)

--- backend/src/main/java/com/but/rebloom/achievement/repository/AchievementRepository.java
+++ backend/src/main/java/com/but/rebloom/achievement/repository/AchievementRepository.java
@@ -2,6 +2,7 @@ package com.but.rebloom.achievement.repository;
 
 import com.but.rebloom.achievement.domain.Achievement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,6 +13,7 @@ public interface AchievementRepository extends JpaRepository<Achievement, Long> 
     // 업적 조회 함수
     Optional<Achievement> findAchievementByAchievementId(Long achievementId);
     Optional<Achievement> findAchievementByAchievementTitle(String achievementTitle);
+    @Query("SELECT a FROM Achievement a")
     Optional<List<Achievement>> findAllAchievements();
 
     // 존재 유무 확인 함수
