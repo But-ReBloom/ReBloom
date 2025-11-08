8845186589978b390f4ecef11132bc47cd62bd62
황정빈 <hjbin1211@gmail.com>
2025-11-09 01:04:19 +0900

    Revert "fix : Achievement re-mapping"
    This reverts commit 90b7369c1e6e6b8ff062f0bd5463c325236e33c6.

--- backend/src/main/java/com/but/rebloom/achievement/domain/Achievement.java
+++ backend/src/main/java/com/but/rebloom/achievement/domain/Achievement.java
@@ -21,7 +21,7 @@ public class Achievement {
     @Column(name = "achieve_id", unique = true, nullable = false)
     private Long achievementId;
 
-    @Column(name = "title", nullable = false)
+    @Column(name = "title", unique = true, nullable = false)
     private String achievementTitle;
 
     @Column(name = "description", nullable = false)
