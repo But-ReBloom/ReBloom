ce1a373ef3329e33f6dac56eb4516c9cda5aaa73
황정빈 <hjbin1211@gmail.com>
2025-11-14 23:47:36 +0900

    refactor : Fix AchievementRepository followed from the convention

--- backend/src/main/java/com/but/rebloom/achievement/repository/AchievementRepository.java
+++ backend/src/main/java/com/but/rebloom/achievement/repository/AchievementRepository.java
@@ -11,21 +11,42 @@ import java.util.Optional;
 
 @Repository
 public interface AchievementRepository extends JpaRepository<Achievement, Long> {
-    // 업적 조회 함수
-    Optional<Achievement> findAchievementByAchievementId(Long achievementId);
-    Optional<Achievement> findAchievementByAchievementTitle(String achievementTitle);
+    // 업적 조회 - achievementId
+    @Query("select a from Achievement a where a.achievementId = :achievementId")
+    Optional<Achievement> findAchievementByAchievementId(@Param("achievementId") Long achievementId);
+
+    // 업적 조회 - achievementTitle
+    @Query("select a from Achievement a where a.achievementTitle = :achievementTitle")
+    Optional<Achievement> findAchievementByAchievementTitle(
+            @Param("achievementTitle") String achievementTitle
+    );
+
+    // 업적 조회 - 전체
     @Query("select a from Achievement a")
     Optional<List<Achievement>> findAllAchievements();
 
-    // 존재 유무 확인 함수
-    Boolean existsByAchievementId(Long achievementId);
-    Boolean existsByAchievementTitle(String achievementTitle);
+    // 존재 유무 확인 함수 - achievementId
+    @Query("""
+        select case when count(a) > 0 then true else false end
+        from Achievement a
+        where a.achievementId = :achievementId
+    """)
+    boolean existsByAchievementId(@Param("achievementId") Long achievementId);
+
+    // 존재 유무 확인 함수 - achievementTitle
+    @Query("""
+        select case when count(a) > 0 then true else false end
+        from Achievement a
+        where a.achievementTitle = :achievementTitle
+    """)
+    boolean existsByAchievementTitle(@Param("achievementTitle") String achievementTitle);
+
     @Query("""
         select case when count(a) > 0 then true else false end
         from Achievement a
         where a.achievementId = :achievementId and trim(a.achievementTitle) = :achievementTitle
     """)
-    Boolean existsByAchievementIdAndAchievementTitle(
+    boolean existsByAchievementIdAndAchievementTitle(
             @Param("achievementId") Long achievementId,
             @Param("achievementTitle") String achievementTitle
     );
