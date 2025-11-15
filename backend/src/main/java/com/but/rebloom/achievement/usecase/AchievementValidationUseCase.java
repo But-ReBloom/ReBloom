daff81dd9a90bc59543311acda54e109353d7b1c
황정빈 <hjbin1211@gmail.com>
2025-11-15 22:20:23 +0900

    refactor : UserAchievement DI fix

--- backend/src/main/java/com/but/rebloom/achievement/usecase/AchievementValidationUseCase.java
+++ backend/src/main/java/com/but/rebloom/achievement/usecase/AchievementValidationUseCase.java
@@ -2,22 +2,14 @@ package com.but.rebloom.achievement.usecase;
 
 import com.but.rebloom.achievement.exception.AchievementNotFoundException;
 import com.but.rebloom.achievement.repository.AchievementRepository;
-import com.but.rebloom.common.usecase.ValidationUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AchievementValidationUseCase {
-    // DI
-    private final ValidationUseCase validationUseCase;
     // 디비 접근
     private final AchievementRepository achievementRepository;
-
-    // 널 값 확인 - 한 요소
-    public <T> void checkNull(T element) {
-        validationUseCase.checkNull(element);
-    }
 
     // 존재하는지 확인 - 업적 아이디
     public void checkExistAchievementByAchievementId(Long achievementId) {
