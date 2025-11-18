ff86f374b5f97828aaecba7e2fa67820b5be96c1
황정빈 <hjbin1211@gmail.com>
2025-11-18 09:10:14 +0900

    hotfix : Add Jpa extension in TierRepository

--- backend/src/main/java/com/but/rebloom/auth/repository/TierRepository.java
+++ backend/src/main/java/com/but/rebloom/auth/repository/TierRepository.java
@@ -1,5 +1,7 @@
 package com.but.rebloom.auth.repository;
 
+import com.but.rebloom.auth.domain.Tier;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -7,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface TierRepository {
+public interface TierRepository extends JpaRepository<Tier, Long> {
     // tierPoint에 따른 티어 확인
     @Query("""
         select t.tierName
