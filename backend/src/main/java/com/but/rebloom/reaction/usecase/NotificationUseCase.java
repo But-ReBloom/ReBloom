123d4516720ca968ca3c268bb6b2cdf45dc73c43
이도연 <dgsw0509@dgsw.hs.kr>
2025-11-18 20:27:28 +0900

    feat : email sending parent class

--- backend/src/main/java/com/but/rebloom/reaction/usecase/NotificationUseCase.java
+++ backend/src/main/java/com/but/rebloom/reaction/usecase/NotificationUseCase.java
@@ -1,49 +1,36 @@
 package com.but.rebloom.reaction.usecase;
 
+import com.but.rebloom.common.usecase.EmailSenderUseCase;
 import com.but.rebloom.reaction.dto.request.CommentNotificationRequest;
 import com.but.rebloom.reaction.dto.request.HeartNotificationRequest;
-import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
-public class NotificationUseCase {
-    private final JavaMailSender mailSender;
+public class NotificationUseCase extends EmailSenderUseCase {
+
+    // 부모 생성자 호출
+    public NotificationUseCase(JavaMailSender mailSender) {
+        super(mailSender);
+    }
 
     public void sendCommentNotification(CommentNotificationRequest commentNotificationRequest) {
-        try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setTo(commentNotificationRequest.getOwnerEmail());
-            message.setSubject("[ReBloom] 내 글에 새로운 댓글이 달렸습니다.");
-            message.setText(String.format(
-                    "%s님, \n%s님이 회원님의 게시글에 댓글을 남겼습니다.\n댓글 내용: %s\n지금 바로 ReBloom에서 확인해보세요!",
-                    commentNotificationRequest.getOwnerUserId(),
-                    commentNotificationRequest.getCommenterUserId(),
-                    commentNotificationRequest.getCommentContent())
-            );
-            mailSender.send(message);
-        } catch (Exception e){
-            e.printStackTrace();
-            throw new RuntimeException("전송 중 오류: " + e.getMessage());
-        }
+        String subject = "[ReBloom] 내 글에 새로운 댓글이 달렸습니다.";
+        String text = String.format(
+                "%s님, \n%s님이 회원님의 게시글에 댓글을 남겼습니다.\n댓글 내용: %s\n지금 바로 ReBloom에서 확인해보세요!",
+                commentNotificationRequest.getOwnerUserId(),
+                commentNotificationRequest.getCommenterUserId(),
+                commentNotificationRequest.getCommentContent()
+        );
+        sendEmail(commentNotificationRequest.getOwnerEmail(), subject, text);
     }
 
     public void sendHeartNotification(HeartNotificationRequest heartNotificationRequest) {
-        try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setTo(heartNotificationRequest.getOwnerEmail());
-            message.setSubject("[ReBloom] 내 글에 좋아요가 달렸습니다.");
-            message.setText(String.format(
-                    "%s님, \n%s님이 회원님의 게시글에 좋아요를 남겼습니다.",
-                    heartNotificationRequest.getOwnerUserId(),
-                    heartNotificationRequest.getLikerUserId())
-            );
-            mailSender.send(message);
-        } catch (Exception e){
-            e.printStackTrace();
-            throw new RuntimeException("전송 중 오류: " + e.getMessage());
-        }
+        String subject = "[ReBloom] 내 글에 좋아요가 달렸습니다.";
+        String text = String.format(
+                "%s님, \n%s님이 회원님의 게시글에 좋아요를 남겼습니다.",
+                heartNotificationRequest.getOwnerUserId(),
+                heartNotificationRequest.getLikerUserId()
+        );
     }
 }
