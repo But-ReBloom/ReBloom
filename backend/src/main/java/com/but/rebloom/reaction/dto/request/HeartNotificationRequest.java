10f12f415a8c9ac81054934905e12b700d06f8a0
이도연 <dgsw0509@dgsw.hs.kr>
2025-11-18 17:07:54 +0900

    feat : send noti
    # Conflicts:
    #	backend/src/main/java/com/but/rebloom/reaction/dto/request/CommentNotificationRequest.java
    #	backend/src/main/java/com/but/rebloom/reaction/dto/request/HeartNotificationRequest.java
    #	backend/src/main/java/com/but/rebloom/reaction/usecase/CommentUseCase.java

--- backend/src/main/java/com/but/rebloom/reaction/dto/request/HeartNotificationRequest.java
+++ backend/src/main/java/com/but/rebloom/reaction/dto/request/HeartNotificationRequest.java
@@ -3,10 +3,7 @@ package com.but.rebloom.reaction.dto.request;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
-
 @Getter
-@Setter
 @Builder
 public class HeartNotificationRequest {
     @NotNull
