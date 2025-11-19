e873bf515f2ea7174b2067da0eadad6df330fa39
황정빈 <hjbin1211@gmail.com>
2025-11-19 12:16:47 +0900

    refactor : Fix jpa method in ActivityRepository

--- backend/src/main/java/com/but/rebloom/channel/repository/ChannelRepository.java
+++ backend/src/main/java/com/but/rebloom/channel/repository/ChannelRepository.java
@@ -33,6 +33,4 @@ public interface ChannelRepository extends JpaRepository<Channel, Long> {
     // 관리자 승인/거절 처리 (일주일 이내)
     @Query("select c from Channel c where c.isAccepted = false and c.channelCreatedAt < :deadline")
     List<Channel> findPendingChannelsOlderThan(@Param("deadline") LocalDateTime deadline);
-
-
 }
