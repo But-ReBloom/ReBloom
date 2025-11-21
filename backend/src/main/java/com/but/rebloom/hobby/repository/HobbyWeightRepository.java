f76fdb12672906e94b97baabdbbd5b11f0889255
황정빈 <hjbin1211@gmail.com>
2025-11-22 03:11:21 +0900

    refactor : Add jpa method that 3 hobby weights linked with the category

--- backend/src/main/java/com/but/rebloom/hobby/repository/HobbyWeightRepository.java
+++ backend/src/main/java/com/but/rebloom/hobby/repository/HobbyWeightRepository.java
@@ -152,4 +152,134 @@ public interface HobbyWeightRepository extends JpaRepository<HobbyWeight, Long> 
         limit 1
     """, nativeQuery = true)
     Optional<HobbyWeight> findByHobbyWeightCreativity(@Param("h_w_creativity") double hobbyWeightCreativity);
+
+    // 취미 가중치 조회 - Social 기준
+    @Query(value = """
+        with candidates as (
+            (
+                select *
+                from HobbyWeight
+                where hobbyWeightSocial >= :h_w_social
+                order by hobbyWeightSocial asc
+                limit 3
+            )
+            union all
+            (
+                select *
+                from HobbyWeight
+                where hobbyWeightSocial < :h_w_social
+                order by hobbyWeightSocial desc
+                limit 3
+            )
+        )
+        select *
+        from candidates
+        order by abs(hobbyWeightSocial - :h_w_social) asc
+        limit 3
+    """, nativeQuery = true)
+    Optional<HobbyWeight> findAllByHobbyWeightSocial(@Param("h_w_social") double hobbyWeightSocial);
+
+    // 취미 가중치 조회 - Learning 기준
+    @Query(value = """
+        with candidates as (
+            (
+                select *
+                from HobbyWeight
+                where hobbyWeightLearning >= :h_w_learning
+                order by hobbyWeightLearning asc
+                limit 3
+            )
+            union all
+            (
+                select *
+                from HobbyWeight
+                where hobbyWeightLearning < :h_w_learning
+                order by hobbyWeightLearning desc
+                limit 3
+            )
+        )
+        select *
+        from candidates
+        order by abs(hobbyWeightLearning - :h_w_learning) asc
+        limit 3
+    """, nativeQuery = true)
+    Optional<HobbyWeight> findAllByHobbyWeightLearning(@Param("h_w_learning") double hobbyWeightLearning);
+
+    // 취미 가중치 조회 - Planning 기준
+    @Query(value = """
+        with candidates as (
+            (
+                select *
+                from HobbyWeight
+                where hobbyWeightPlanning >= :h_w_planning
+                order by hobbyWeightPlanning asc
+                limit 3
+            )
+            union all
+            (
+                select *
+                from HobbyWeight
+                where hobbyWeightPlanning < :h_w_planning
+                order by hobbyWeightPlanning desc
+                limit 3
+            )
+        )
+        select *
+        from candidates
+        order by abs(hobbyWeightPlanning - :h_w_planning) asc
+        limit 3
+    """, nativeQuery = true)
+    Optional<HobbyWeight> findAllByHobbyWeightPlanning(@Param("h_w_planning") double hobbyWeightPlanning);
+
+    // 취미 가중치 조회 - Focus 기준
+    @Query(value = """
+        with candidates as (
+            (
+                select *
+                from HobbyWeight
+                where hobbyWeightFocus >= :h_w_focus
+                order by hobbyWeightFocus asc
+                limit 3
+            )
+            union all
+            (
+                select *
+                from HobbyWeight
+                where hobbyWeightFocus < :h_w_focus
+                order by hobbyWeightFocus desc
+                limit 3
+            )
+        )
+        select *
+        from candidates
+        order by abs(hobbyWeightFocus - :h_w_focus) asc
+        limit 3
+    """, nativeQuery = true)
+    Optional<HobbyWeight> findAllByHobbyWeightFocus(@Param("h_w_focus") double hobbyWeightFocus);
+
+    // 취미 가중치 조회 - Creativity 기준
+    @Query(value = """
+        with candidates as (
+            (
+                select *
+                from HobbyWeight
+                where hobbyWeightCreativity >= :h_w_creativity
+                order by hobbyWeightCreativity asc
+                limit 3
+            )
+            union all
+            (
+                select *
+                from HobbyWeight
+                where hobbyWeightCreativity < :h_w_creativity
+                order by hobbyWeightCreativity desc
+                limit 3
+            )
+        )
+        select *
+        from candidates
+        order by abs(hobbyWeightCreativity - :h_w_creativity) asc
+        limit 3
+    """, nativeQuery = true)
+    Optional<HobbyWeight> findAllByHobbyWeightCreativity(@Param("h_w_creativity") double hobbyWeightCreativity);
 }
