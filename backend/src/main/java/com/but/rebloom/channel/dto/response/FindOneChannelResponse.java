1520249de51157ca434e8d3a8d3a1dc82647bcae
황정빈 <hjbin1211@gmail.com>
2025-11-25 19:57:55 +0900

    refactor : Add userTier in channel

--- backend/src/main/java/com/but/rebloom/channel/dto/response/FindOneChannelResponse.java
+++ backend/src/main/java/com/but/rebloom/channel/dto/response/FindOneChannelResponse.java
@@ -1,8 +1,10 @@
 package com.but.rebloom.channel.dto.response;
 
+import com.but.rebloom.auth.domain.TierName;
 import com.but.rebloom.channel.domain.Channel;
 import com.but.rebloom.hobby.repository.ActivityRepository;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -13,6 +15,10 @@ import java.util.Map;
 public class FindOneChannelResponse {
     @NotBlank
     private String channelName;
+    @NotBlank
+    private String userId;
+    @NotNull
+    private TierName userTier;
     @NotBlank
     private String channelIntro;
     @NotBlank
@@ -25,6 +31,8 @@ public class FindOneChannelResponse {
 
         return FindOneChannelResponse.builder()
                 .channelName(channel.getChannelTitle())
+                .userId(channel.getUser().getUserId())
+                .userTier(channel.getUser().getUserTier())
                 .channelIntro(channel.getChannelIntro())
                 .linkedHobbyName(linkedHobbyName)
                 .build();
