2cb6bb08c0d4dfbeea3e67d75e2bea7484c36df2
zerith <me.secur1ty.dev@gmail.com>
2025-12-01 13:30:47 +0900

    fix : 도커 배포 파일 수정

--- backend/src/main/java/com/but/rebloom/config/OpenApiConfig.java
+++ backend/src/main/java/com/but/rebloom/config/OpenApiConfig.java
@@ -1,6 +1,10 @@
 package com.but.rebloom.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,9 +17,20 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .info(new Info()
+                        .title("Rebloom API")
+                        .description("Rebloom API Documentation")
+                        .version("1.0.0"))
                 .servers(List.of(
                         new Server().url("https://api.rebloom.co.kr").description("Production Server"),
                         new Server().url("http://localhost:8080").description("Local Server")
-                ));
+                ))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
