d9f8941232690579b795522b7dafc4df0b6f20ba
zerith <me.secur1ty.dev@gmail.com>
2025-12-01 13:39:01 +0900

    fix : 도커 배포 파일 수정

--- backend/src/main/java/com/but/rebloom/config/SwaggerConfig.java
+++ backend/src/main/java/com/but/rebloom/config/SwaggerConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI rebloomOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("Rebloom API")
