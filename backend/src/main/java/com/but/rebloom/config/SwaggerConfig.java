5f190f7294da91adba8ab61d4c99374da551c03c
zerith <me.secur1ty.dev@gmail.com>
2025-12-01 13:35:17 +0900

    fix : 도커 배포 파일 수정

--- backend/src/main/java/com/but/rebloom/config/SwaggerConfig.java
+++ backend/src/main/java/com/but/rebloom/config/SwaggerConfig.java
@@ -5,15 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
-
 @Configuration
-public class OpenApiConfig {
-
+public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
@@ -21,10 +17,6 @@ public class OpenApiConfig {
                         .title("Rebloom API")
                         .description("Rebloom API Documentation")
                         .version("1.0.0"))
-                .servers(List.of(
-                        new Server().url("https://api.rebloom.co.kr").description("Production Server"),
-                        new Server().url("http://localhost:8080").description("Local Server")
-                ))
                 .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
                 .components(new Components()
                         .addSecuritySchemes("Bearer Authentication",
