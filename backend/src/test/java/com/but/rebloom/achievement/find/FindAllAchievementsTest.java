b30528d13fc14c468508373d24a308d39909dede
황정빈 <hjbin1211@gmail.com>
2025-12-03 02:24:55 +0900

    test : Add findAllAchievementsFailByAchievementNotFoundTest logic

--- backend/src/test/java/com/but/rebloom/achievement/find/FindAllAchievementsTest.java
+++ backend/src/test/java/com/but/rebloom/achievement/find/FindAllAchievementsTest.java
@@ -1,6 +1,7 @@
 package com.but.rebloom.achievement.find;
 
 import com.but.rebloom.domain.achievement.domain.Achievement;
+import com.but.rebloom.domain.achievement.exception.AchievementNotFoundException;
 import com.but.rebloom.domain.achievement.repository.AchievementRepository;
 import com.but.rebloom.domain.achievement.usecase.DefaultAchievementUseCase;
 import org.junit.jupiter.api.DisplayName;
@@ -13,6 +14,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -41,5 +43,13 @@ public class FindAllAchievementsTest {
 
         // Then
         assertThat(achievements).isNotEmpty();
+    }
+
+    @Test
+    @DisplayName("전체 업적 조회 - 업적 조회 실패로 인한 실패")
+    public void findAllAchievementsFailByAchievementNotFoundTest() {
+        // Given & When & Then
+        assertThrows(AchievementNotFoundException.class,
+                () -> defaultAchievementUseCase.findAllAchievements());
     }
 }
