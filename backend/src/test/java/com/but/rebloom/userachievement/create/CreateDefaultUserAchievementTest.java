4b7f2bc98c831a0d06a5f7ccf789a49cdbf8d659
황정빈 <hjbin1211@gmail.com>
2025-12-04 17:38:20 +0900

    test : Add createDefaultUserAchievementSuccessTest logic

---
+++ backend/src/test/java/com/but/rebloom/userachievement/create/CreateDefaultUserAchievementTest.java
@@ -0,0 +1,54 @@
+package com.but.rebloom.userachievement.create;
+
+import com.but.rebloom.domain.achievement.domain.Achievement;
+import com.but.rebloom.domain.achievement.domain.UserAchievement;
+import com.but.rebloom.domain.achievement.repository.AchievementRepository;
+import com.but.rebloom.domain.achievement.repository.UserAchievementRepository;
+import com.but.rebloom.domain.achievement.usecase.DefaultUserAchievementUseCase;
+import com.but.rebloom.domain.auth.domain.Provider;
+import com.but.rebloom.domain.auth.domain.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+public class CreateDefaultUserAchievementTest {
+    @Mock
+    private AchievementRepository achievementRepository;
+    @Mock
+    private UserAchievementRepository userAchievementRepository;
+    @InjectMocks
+    private DefaultUserAchievementUseCase defaultUserAchievementUseCase;
+
+    @Test
+    @DisplayName("유저 업적 생성")
+    public void createDefaultUserAchievementSuccessTest() {
+        // Given
+        String userEmail = "test@test.com";
+
+        Achievement mockAchievement = Achievement.builder()
+                .achievementId(1L)
+                .achievementTitle("test achievement title")
+                .build();
+
+        List<Achievement> mockAchievements = List.of(mockAchievement);
+
+        when(achievementRepository.findAll())
+                .thenReturn(mockAchievements);
+        when(userAchievementRepository.saveAll(anyList()))
+                .thenReturn(null); // 어짜피 저장은 안해서 null로 해둠
+
+        // When & Then
+        defaultUserAchievementUseCase.createDefaultUserAchievement(userEmail);
+    }
+}
