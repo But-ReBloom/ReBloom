0e64421bc2925a0e3d88c3071879242c1c6ac325
황정빈 <hjbin1211@gmail.com>
2025-12-04 18:04:47 +0900

    test : Add updateUserAchievementToSuccessSuccessIfNotSucceedInAdvanceTest logic

--- backend/src/test/java/com/but/rebloom/userachievement/update/UpdateUserAchievementToSuccessTest.java
+++ backend/src/test/java/com/but/rebloom/userachievement/update/UpdateUserAchievementToSuccessTest.java
@@ -5,6 +5,9 @@ import com.but.rebloom.domain.achievement.domain.UserAchievement;
 import com.but.rebloom.domain.achievement.repository.AchievementRepository;
 import com.but.rebloom.domain.achievement.repository.UserAchievementRepository;
 import com.but.rebloom.domain.achievement.usecase.DefaultUserAchievementUseCase;
+import com.but.rebloom.domain.auth.domain.Provider;
+import com.but.rebloom.domain.auth.domain.TierName;
+import com.but.rebloom.domain.auth.domain.User;
 import com.but.rebloom.domain.auth.repository.UserRepository;
 import com.but.rebloom.domain.auth.usecase.DefaultTierUseCase;
 import org.junit.jupiter.api.DisplayName;
@@ -16,6 +19,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -49,5 +54,51 @@ public class UpdateUserAchievementToSuccessTest {
 
         // When & Then
         defaultUserAchievementUseCase.updateUserAchievementToSuccess(userEmail, achievementTitle);
+    }
+
+    @Test
+    @DisplayName("유저 업데이트 성공 전환 테스트 - 성공(업적을 깨두지 않은 경우)")
+    public void updateUserAchievementToSuccessSuccessIfNotSucceedInAdvanceTest() {
+        // Given
+        String userEmail = "user@email.com";
+        String achievementTitle = "achievementTitle";
+
+        Achievement mockAchievement = Achievement.builder()
+                .achievementId(1L)
+                .achievementTitle(achievementTitle)
+                .achievementRewardPoint(100)
+                .achievementRewardTierPoint(10)
+                .build();
+
+        User mockUser = User.builder()
+                .userEmail(userEmail)
+                .userPassword("userPasswor123!")
+                .userProvider(Provider.SELF)
+                .userTierPoint(0)
+                .userTier(TierName.IRON)
+                .build();
+
+        UserAchievement mockUserAchievement = UserAchievement.builder()
+                .achievement(mockAchievement)
+                .user(mockUser)
+                .isSuccess(false)
+                .userAchievementProgress(0f)
+                .build();
+
+        when(userAchievementRepository.findByUserEmailAndAchievement_AchievementTitle(userEmail, achievementTitle))
+                .thenReturn(Optional.of(mockUserAchievement));
+        when(achievementRepository.findByAchievementTitle(achievementTitle))
+                .thenReturn(Optional.of(mockAchievement));
+        when(userRepository.findByUserEmail(userEmail))
+                .thenReturn(Optional.of(mockUser));
+        when(defaultTierUseCase.getTierEnumByPoint(anyInt()))
+                .thenReturn(Optional.of(TierName.IRON));
+
+        // When
+        defaultUserAchievementUseCase.updateUserAchievementToSuccess(userEmail, achievementTitle);
+
+        // Then
+        assertThat(mockUserAchievement.getUserAchievementProgress()).isEqualTo(100f);
+        assertThat(mockUserAchievement.getIsSuccess()).isTrue();
     }
 }
