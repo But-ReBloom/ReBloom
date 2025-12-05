7a6736d768b9e3aee5bf22057291e8f940df0434
황정빈 <hjbin1211@gmail.com>
2025-12-05 22:11:44 +0900

    test : Add findChannelFailByChannelNotFoundTest logic

--- backend/src/test/java/com/but/rebloom/channel/find/FindChannelTest.java
+++ backend/src/test/java/com/but/rebloom/channel/find/FindChannelTest.java
@@ -2,6 +2,7 @@ package com.but.rebloom.channel.find;
 
 import com.but.rebloom.domain.channel.domain.Channel;
 import com.but.rebloom.domain.channel.dto.request.SearchChannelRequest;
+import com.but.rebloom.domain.channel.exception.ChannelNotFoundException;
 import com.but.rebloom.domain.channel.repository.ChannelRepository;
 import com.but.rebloom.domain.channel.usecase.ChannelUseCase;
 import org.junit.jupiter.api.DisplayName;
@@ -14,6 +15,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
@@ -42,5 +44,18 @@ public class FindChannelTest {
 
         // Then
         assertThat(channels).isEqualTo(mockChannels);
+    }
+
+    @Test
+    @DisplayName("키워드 조회 테스트 - 채널 조회 실패로 인한 실패")
+    public void findChannelFailByChannelNotFoundTest() {
+        // Given
+        SearchChannelRequest request = new SearchChannelRequest(
+                "keyword"
+        );
+
+        // When & Then
+        assertThrows(ChannelNotFoundException.class,
+                () -> channelUseCase.findChannel(request));
     }
 }
