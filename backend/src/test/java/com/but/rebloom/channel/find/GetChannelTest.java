cee2dbeffeb2b53b9cf8766e268f88054667c170
황정빈 <hjbin1211@gmail.com>
2025-12-07 00:10:48 +0900

    test : Add getChannelFailByChannelNotFoundTest logic

--- backend/src/test/java/com/but/rebloom/channel/find/GetChannelTest.java
+++ backend/src/test/java/com/but/rebloom/channel/find/GetChannelTest.java
@@ -1,8 +1,10 @@
 package com.but.rebloom.channel.find;
 
 import com.but.rebloom.domain.channel.domain.Channel;
+import com.but.rebloom.domain.channel.exception.ChannelNotFoundException;
 import com.but.rebloom.domain.channel.repository.ChannelRepository;
 import com.but.rebloom.domain.channel.usecase.ChannelUseCase;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -38,5 +40,16 @@ public class GetChannelTest {
 
         // Then
         assertThat(channel).isEqualTo(mockChannel);
+    }
+
+    @Test
+    @DisplayName("채널 조회 테스트 - 채널 조회 실패로 인한 실패")
+    public void getChannelFailByChannelNotFoundTest() {
+        // Given
+        Long channelId = 1L;
+
+        // When & Then
+        Assertions.assertThrows(ChannelNotFoundException.class,
+                () -> channelUseCase.getChannel(channelId));
     }
 }
