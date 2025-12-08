34292fd304fbeb86ab998ee13df6113907444ec7
황정빈 <hjbin1211@gmail.com>
2025-12-08 13:39:16 +0900

    test : Add applyMemberVerificationFailByAlreadyAppliedTest logic

--- backend/src/test/java/com/but/rebloom/userchannel/apply/ApplyMemberVerificationTest.java
+++ backend/src/test/java/com/but/rebloom/userchannel/apply/ApplyMemberVerificationTest.java
@@ -6,6 +6,7 @@ import com.but.rebloom.domain.channel.domain.Channel;
 import com.but.rebloom.domain.channel.domain.UserChannel;
 import com.but.rebloom.domain.channel.domain.VerifyStatus;
 import com.but.rebloom.domain.channel.dto.request.ApplyMemberRequest;
+import com.but.rebloom.domain.channel.exception.AlreadyUsingUserChannelException;
 import com.but.rebloom.domain.channel.exception.ChannelNotFoundException;
 import com.but.rebloom.domain.channel.repository.ChannelRepository;
 import com.but.rebloom.domain.channel.repository.UserChannelRepository;
@@ -87,11 +88,41 @@ public class ApplyMemberVerificationTest {
         User mockUser = User.builder()
                 .userEmail("test@test.com")
                 .build();
+
         when(findCurrentUserUseCase.getCurrentUser())
                 .thenReturn(mockUser);
 
         // When & Then
         assertThrows(ChannelNotFoundException.class,
+                () -> verifyUserUseCase.applyMemberVerification(applyMemberRequest));
+    }
+
+    @Test
+    @DisplayName("채널 가입 신청 테스트 - 이미 지원한 채널로 실패")
+    public void applyMemberVerificationFailByAlreadyAppliedTest() {
+        // Given
+        ApplyMemberRequest applyMemberRequest = new ApplyMemberRequest(
+                1L,
+                "applyMessage"
+        );
+
+        User mockUser = User.builder()
+                .userEmail("test@test.com")
+                .build();
+
+        Channel mockChannel = Channel.builder()
+                .channelId(1L)
+                .build();
+
+        when(findCurrentUserUseCase.getCurrentUser())
+                .thenReturn(mockUser);
+        when(channelRepository.findByChannelIdAndChannelStatusAccepted(applyMemberRequest.getChannelId()))
+                .thenReturn(Optional.of(mockChannel));
+        when(userChannelRepository.existsByChannel_ChannelIdAndUser_UserEmail(applyMemberRequest.getChannelId(), mockUser.getUserEmail()))
+                .thenReturn(true);
+
+        // When & Then
+        assertThrows(AlreadyUsingUserChannelException.class,
                 () -> verifyUserUseCase.applyMemberVerification(applyMemberRequest));
     }
 }
