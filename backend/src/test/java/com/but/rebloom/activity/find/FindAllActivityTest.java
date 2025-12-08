ba758ce1173c087f13b8200c7adeccc2d25f601b
황정빈 <hjbin1211@gmail.com>
2025-12-08 15:16:23 +0900

    test : Add findAllActivitySuccessTest logic

---
+++ backend/src/test/java/com/but/rebloom/activity/find/FindAllActivityTest.java
@@ -0,0 +1,51 @@
+package com.but.rebloom.activity.find;
+
+import com.but.rebloom.domain.auth.domain.User;
+import com.but.rebloom.domain.auth.usecase.FindCurrentUserUseCase;
+import com.but.rebloom.domain.hobby.domain.Activity;
+import com.but.rebloom.domain.hobby.repository.ActivityRepository;
+import com.but.rebloom.domain.hobby.usecase.DefaultActivityUseCase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class FindAllActivityTest {
+    @Mock
+    private FindCurrentUserUseCase findCurrentUserUseCase;
+    @Mock
+    private ActivityRepository activityRepository;
+    @InjectMocks
+    private DefaultActivityUseCase defaultActivityUseCase;
+
+    @Test
+    @DisplayName("활동 전체 조회 테스트 - 성공")
+    public void findAllActivitySuccessTest() {
+        // Given
+        User mockUser = User.builder()
+                .userEmail("user@email.com")
+                .build();
+
+        Activity mockActivity = Activity.builder().build();
+        List<Activity> mockActivities = List.of(mockActivity);
+
+        when(findCurrentUserUseCase.getCurrentUser())
+                .thenReturn(mockUser);
+        when(activityRepository.findByUser_UserEmail(mockUser.getUserEmail()))
+                .thenReturn(mockActivities);
+
+        // When
+        List<Activity> activities = defaultActivityUseCase.findAllActivity();
+
+        // Then
+        assertThat(activities.size()).isEqualTo(mockActivities.size());
+    }
+}
