ebcdf46ebb9212a3b2a6de17b587a2436b220c56
황정빈 <hjbin1211@gmail.com>
2025-12-08 09:25:31 +0900

    test : Add getApplyUsersByUserEmailFailByNoAuthorityTest logic

--- backend/src/test/java/com/but/rebloom/userchannel/find/GetApplyUsersByUserEmailTest.java
+++ backend/src/test/java/com/but/rebloom/userchannel/find/GetApplyUsersByUserEmailTest.java
@@ -7,6 +7,7 @@ import com.but.rebloom.domain.channel.domain.UserChannel;
 import com.but.rebloom.domain.channel.domain.VerifyStatus;
 import com.but.rebloom.domain.channel.repository.UserChannelRepository;
 import com.but.rebloom.domain.channel.usecase.VerifyUserUseCase;
+import com.but.rebloom.global.exception.NoAuthorityException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,6 +18,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -55,5 +57,24 @@ public class GetApplyUsersByUserEmailTest {
 
         // Then
         assertThat(userChannels.size()).isEqualTo(mockUserChannels.size());
+    }
+
+    @Test
+    @DisplayName("유저의 신청 목록 조회 테스트 - 권한 부족으로 실패")
+    public void getApplyUsersByUserEmailFailByNoAuthorityTest() {
+        // Given
+        String userEmail = "test@test.com";
+
+        User mockUser = User.builder()
+                .userEmail("user@test.com")
+                .userRole(Role.USER)
+                .build();
+
+        when(findCurrentUserUseCase.getCurrentUser())
+                .thenReturn(mockUser);
+
+        // When & Then
+        assertThrows(NoAuthorityException.class,
+                () -> verifyUserUseCase.getApplyUsersByUserEmail(userEmail));
     }
 }
