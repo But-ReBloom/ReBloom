0b6b6d9af0fad7f6f7bebd1542e2a42a61b79315
황정빈 <hjbin1211@gmail.com>
2025-12-08 16:37:45 +0900

    test : Add AddActivityTest logic

--- backend/src/test/java/com/but/rebloom/activity/create/AddActivityTest.java
+++ backend/src/test/java/com/but/rebloom/activity/create/AddActivityTest.java
@@ -1,11 +1,13 @@
 package com.but.rebloom.activity.create;
 
 import com.but.rebloom.domain.auth.domain.User;
+import com.but.rebloom.domain.auth.exception.UserNotFoundException;
 import com.but.rebloom.domain.auth.repository.UserRepository;
 import com.but.rebloom.domain.auth.usecase.FindCurrentUserUseCase;
 import com.but.rebloom.domain.hobby.domain.Activity;
 import com.but.rebloom.domain.hobby.domain.Hobby;
 import com.but.rebloom.domain.hobby.dto.request.AddActivityRequest;
+import com.but.rebloom.domain.hobby.exception.HobbyNotFoundException;
 import com.but.rebloom.domain.hobby.repository.ActivityRepository;
 import com.but.rebloom.domain.hobby.repository.HobbyRepository;
 import com.but.rebloom.domain.hobby.usecase.DefaultActivityUseCase;
@@ -20,6 +22,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
@@ -77,5 +80,57 @@ public class AddActivityTest {
 
         // Then
         assertThat(activity).isEqualTo(mockActivity);
+    }
+
+    @Test
+    @DisplayName("활동 추가 테스트 - 취미 조회 실패로 실패")
+    public void addActivityFailByHobbyNotFoundTest() {
+        // Given
+        AddActivityRequest addActivityRequest = new AddActivityRequest(
+                1L,
+                "useremail@email.com"
+        );
+
+        User mockUser = User.builder()
+                .userEmail(addActivityRequest.getUserEmail())
+                .build();
+
+        when(findCurrentUserUseCase.getCurrentUser())
+                .thenReturn(mockUser);
+        doNothing().when(hobbyValidationUseCase)
+                .checkExistActivityByEmailAndHobby_HobbyId(addActivityRequest.getUserEmail(), addActivityRequest.getHobbyId());
+
+        // When & Then
+        assertThrows(HobbyNotFoundException.class,
+                () -> defaultActivityUseCase.addActivity(addActivityRequest));
+    }
+
+    @Test
+    @DisplayName("활동 추가 테스트 - 유저 조회 실패로 실패")
+    public void addActivityFailByUserNotFoundTest() {
+        // Given
+        AddActivityRequest addActivityRequest = new AddActivityRequest(
+                1L,
+                "useremail@email.com"
+        );
+
+        User mockUser = User.builder()
+                .userEmail(addActivityRequest.getUserEmail())
+                .build();
+
+        Hobby mockHobby = Hobby.builder()
+                .hobbyId(addActivityRequest.getHobbyId())
+                .build();
+
+        when(findCurrentUserUseCase.getCurrentUser())
+                .thenReturn(mockUser);
+        doNothing().when(hobbyValidationUseCase)
+                .checkExistActivityByEmailAndHobby_HobbyId(addActivityRequest.getUserEmail(), addActivityRequest.getHobbyId());
+        when(hobbyRepository.findByHobbyId(addActivityRequest.getHobbyId()))
+                .thenReturn(Optional.of(mockHobby));
+
+        // When & Then
+        assertThrows(UserNotFoundException.class,
+                () -> defaultActivityUseCase.addActivity(addActivityRequest));
     }
 }
