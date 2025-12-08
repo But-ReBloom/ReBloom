897954246fd7fecba8bffc79afd83b566f0e53ef
황정빈 <hjbin1211@gmail.com>
2025-12-08 09:33:11 +0900

    test : Add getApplyUsersByChannelIdAndUserEmailSuccessTest logic

---
+++ backend/src/test/java/com/but/rebloom/userchannel/find/GetApplyUsersByChannelIdAndUserEmailTest.java
@@ -0,0 +1,68 @@
+package com.but.rebloom.userchannel.find;
+
+import com.but.rebloom.domain.auth.domain.Role;
+import com.but.rebloom.domain.auth.domain.User;
+import com.but.rebloom.domain.auth.usecase.FindCurrentUserUseCase;
+import com.but.rebloom.domain.channel.domain.Channel;
+import com.but.rebloom.domain.channel.domain.UserChannel;
+import com.but.rebloom.domain.channel.repository.ChannelRepository;
+import com.but.rebloom.domain.channel.repository.UserChannelRepository;
+import com.but.rebloom.domain.channel.usecase.VerifyUserUseCase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class GetApplyUsersByChannelIdAndUserEmailTest {
+    @Mock
+    private FindCurrentUserUseCase findCurrentUserUseCase;
+    @Mock
+    private ChannelRepository channelRepository;
+    @Mock
+    private UserChannelRepository userChannelRepository;
+    @InjectMocks
+    private VerifyUserUseCase verifyUserUseCase;
+
+    @Test
+    @DisplayName("유저 신청 목록 조회 테스트 - 성공")
+    public void getApplyUsersByChannelIdAndUserEmailSuccessTest() {
+        // Given
+        Long channelId = 1L;
+        String userEmail = "test@test.com";
+
+        User mockUser = User.builder()
+                .userEmail(userEmail)
+                .userRole(Role.USER)
+                .build();
+
+        Channel mockChannel = Channel.builder()
+                .user(mockUser)
+                .build();
+
+        UserChannel mockUserChannel = UserChannel.builder()
+                .user(mockUser)
+                .channel(mockChannel)
+                .build();
+
+        when(findCurrentUserUseCase.getCurrentUser())
+                .thenReturn(mockUser);
+        when(channelRepository.findByChannelIdAndChannelStatusAccepted(channelId))
+                .thenReturn(Optional.of(mockChannel));
+        when(userChannelRepository.findByChannel_ChannelIdAndUser_UserEmail(channelId, userEmail))
+                .thenReturn(Optional.of(mockUserChannel));
+
+        // When
+        UserChannel userChannel = verifyUserUseCase.getApplyUsersByChannelIdAndUserEmail(channelId, userEmail);
+
+        // Then
+        assertThat(userChannel).isEqualTo(mockUserChannel);
+    }
+}
