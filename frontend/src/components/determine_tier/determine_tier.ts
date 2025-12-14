bcdb32404dd250e391dda6b431b8284de5b6e91b
오용준 <dhdydwns723@gmail.com>
2025-12-14 19:27:04 +0900

    fix : tier process

--- frontend/src/components/determine_tier/determine_tier.ts
+++ frontend/src/components/determine_tier/determine_tier.ts
@@ -2,9 +2,9 @@ import iron from "../../assets/images/iron.png";
 import bronze from "../../assets/images/bronze.png";
 import silver from "../../assets/images/silver.png";
 import gold from "../../assets/images/gold.png";
-import platinum from "../../assets/images/platinum.png";
-import diamond from "../../assets/images/master.png";
-import master from "../../assets/images/grandmaster.png";
+import diamond from "../../assets/images/platinum.png";
+import master from "../../assets/images/master.png";
+import challenger from "../../assets/images/grandmaster.png";
 
 /* ===============================
    함수: 티어 이미지 결정
@@ -19,12 +19,12 @@ export function ImageOfTier(tier: string) {
       return silver;
     case "gold":
       return gold;
-    case "platinum":
-      return platinum;
     case "diamond":
       return diamond;
     case "master":
       return master;
+    case "challenger":
+      return challenger;
     default:
       return iron; // 기본값
   }
