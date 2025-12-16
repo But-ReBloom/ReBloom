1f0a9a98361b9e0487adacc92dc9738e1af193d9
zerith <me.secur1ty.dev@gmail.com>
2025-12-16 17:47:06 +0900

    fix : 프론트 배포

--- backend/src/main/java/com/but/rebloom/global/config/CorsConfig.java
+++ backend/src/main/java/com/but/rebloom/global/config/CorsConfig.java
@@ -2,36 +2,36 @@ package com.but.rebloom.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins(
-                                "http://localhost:5500",
-                                "http://localhost:3000",
-                                "http://localhost:5173",
-                                "http://localhost:8080",
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:5500",
+                "http://localhost:3000",
+                "http://localhost:5173",
+                "http://localhost:8080",
+                "http://rebloom.co.kr",
+                "http://www.rebloom.co.kr",
+                "http://api.rebloom.co.kr",
+                "https://rebloom.co.kr",
+                "https://www.rebloom.co.kr",
+                "https://api.rebloom.co.kr"
+        ));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
 
-                                "http://rebloom.co.kr",
-                                "http://www.rebloom.co.kr",
-                                "http://api.rebloom.co.kr",
-
-                                "https://rebloom.co.kr",
-                                "https://www.rebloom.co.kr",
-                                "https://api.rebloom.co.kr"
-
-                        )
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowCredentials(true);
-            }
-        };
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
