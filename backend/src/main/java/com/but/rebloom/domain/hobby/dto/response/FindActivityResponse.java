d88af3dc7efea4b71dfb2a4c7806687e94cc2513
황정빈 <hjbin1211@gmail.com>
2025-12-18 09:29:13 +0900

    hotfix : Fix FindActivityResponse

--- backend/src/main/java/com/but/rebloom/domain/hobby/dto/response/FindActivityResponse.java
+++ backend/src/main/java/com/but/rebloom/domain/hobby/dto/response/FindActivityResponse.java
@@ -1,6 +1,7 @@
 package com.but.rebloom.domain.hobby.dto.response;
 
 import com.but.rebloom.domain.hobby.domain.Activity;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,17 +12,26 @@ import java.time.LocalDate;
 @Builder
 public class FindActivityResponse {
     @NotNull
+    private Long activityId;
+    @NotNull
     private String activityName;
     @NotNull
     private LocalDate activityStart;
     @NotNull
     private LocalDate activityRecent;
+    @NotNull
+    private Long linkedHobbyId;
+    @NotBlank
+    private String linkedHobbyName;
 
     public static FindActivityResponse from(Activity activity) {
         return FindActivityResponse.builder()
+                .activityId(activity.getActivityId())
                 .activityName(activity.getHobby().getHobbyName())
                 .activityStart(activity.getActivityStart())
                 .activityRecent(activity.getActivityRecent())
+                .linkedHobbyId(activity.getHobby().getHobbyId())
+                .linkedHobbyName(activity.getHobby().getHobbyName())
                 .build();
     }
 }
